bf701494137492f958e9ee415270672054221790
NarekXachatryan <50409595+NarekXachatryan@users.noreply.github.com>
2019-05-20 18:52:46 +0400

    Add files via upload

---
+++ Abstract Factory/Arbalet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class Arbalet : Weapon
+    {
+        public override void Hit()
+        {
+            Console.WriteLine("Стреляем из арбалета");
+        }
+    }
+    class Sword : Weapon
+    {
+        public override void Hit()
+        {
+            Console.WriteLine("Бьем мечом");
+        }
+    }
+
+    class FlyMovement : Movement
+    {
+        public override void Move()
+        {
+            Console.WriteLine("Летим");
+        }
+    }
+
+    class RunMovement : Movement
+    {
+        public override void Move()
+        {
+            Console.WriteLine("Бежим");
+        }
+    }
+}
---
+++ Abstract Factory/HeroFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    abstract class HeroFactory
+    {
+        public abstract Movement CreateMovement();
+        public abstract Weapon CreateWeapon();
+    }
+
+    class ElfFactory : HeroFactory
+    {
+        public override Movement CreateMovement()
+        {
+            return new FlyMovement();
+        }
+
+        public override Weapon CreateWeapon()
+        {
+            return new Arbalet();
+        }
+    }
+
+    class VoinFactory : HeroFactory
+    {
+        public override Movement CreateMovement()
+        {
+            return new RunMovement();
+        }
+
+        public override Weapon CreateWeapon()
+        {
+            return new Sword();
+        }
+    }
+}
