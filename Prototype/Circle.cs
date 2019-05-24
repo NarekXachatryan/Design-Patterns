3b5ef479db9d12ba57b431c0402957483ffd4981
NarekXachatryan <50409595+NarekXachatryan@users.noreply.github.com>
2019-05-24 14:43:59 +0400

    Prototype

---
+++ Prototype/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    interface IFigure
+    {
+        IFigure Clone();
+        void GetInfo();
+    }
+    class Circle : IFigure
+    {
+        int radius;
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public IFigure Clone()
+        {
+            return new Circle(this.radius);
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("Круг радиусом { 0}", radius);
+        }
+    }
+
+    
+}
---
+++ Prototype/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Rectangle : IFigure
+    {
+        int width;
+        int height;
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public Rectangle(int id) 
+        {
+
+        }
+        public  IFigure Clone()
+        {
+            return new Rectangle(this.width,this.height);
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("Прямоугольник длиной {0} и шириной {1}", height, width);
+        }
+    }
+}
